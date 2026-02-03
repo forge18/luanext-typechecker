d326ce08b0dffcb92dcfae103e1c9435e4c8990b
forge18 <ndurham@gmail.com>
2026-02-03 13:05:25 -0600

    feat: implement TypeCheckContext and TypeCheckContextImpl for type checking support

---
+++ src/core/context.rs
@@ -0,0 +1,92 @@
+use crate::cli::diagnostics::DiagnosticHandler;
+use crate::core::type_environment::TypeEnvironment;
+use crate::utils::symbol_table::{SymbolKind, SymbolTable};
+use crate::visitors::{AccessControl, TypeNarrower};
+use rustc_hash::FxHashMap;
+use std::sync::Arc;
+use typedlua_parser::ast::statement::{ClassDeclaration, TypeParameter};
+use typedlua_parser::string_interner::CommonIdentifiers;
+use typedlua_parser::string_interner::StringInterner;
+
+pub trait TypeCheckContext {
+    fn symbol_table(&mut self) -> &mut SymbolTable;
+    fn type_env(&mut self) -> &mut TypeEnvironment;
+    fn narrowing(&mut self) -> &mut TypeNarrower;
+    fn access_control(&mut self) -> &mut AccessControl;
+    fn interner(&self) -> &StringInterner;
+    fn common(&self) -> &CommonIdentifiers;
+    fn diagnostic_handler(&self) -> &Arc<dyn DiagnosticHandler>;
+}
+
+#[derive(Debug)]
+pub struct TypeCheckContextImpl {
+    pub symbol_table: SymbolTable,
+    pub type_env: TypeEnvironment,
+    pub narrowing: TypeNarrower,
+    pub access_control: AccessControl,
+    pub interner: Arc<StringInterner>,
+    pub common: Arc<CommonIdentifiers>,
+    pub diagnostic_handler: Arc<dyn DiagnosticHandler>,
+}
+
+impl TypeCheckContextImpl {
+    pub fn new(
+        interner: Arc<StringInterner>,
+        common: Arc<CommonIdentifiers>,
+        diagnostic_handler: Arc<dyn DiagnosticHandler>,
+    ) -> Self {
+        Self {
+            symbol_table: SymbolTable::new(),
+            type_env: TypeEnvironment::new(),
+            narrowing: TypeNarrower::new(),
+            access_control: AccessControl::new(),
+            interner,
+            common,
+            diagnostic_handler,
+        }
+    }
+}
+
+impl TypeCheckContext for TypeCheckContextImpl {
+    fn symbol_table(&mut self) -> &mut SymbolTable {
+        &mut self.symbol_table
+    }
+
+    fn type_env(&mut self) -> &mut TypeEnvironment {
+        &mut self.type_env
+    }
+
+    fn narrowing(&mut self) -> &mut TypeNarrower {
+        &mut self.narrowing
+    }
+
+    fn access_control(&mut self) -> &mut AccessControl {
+        &mut self.access_control
+    }
+
+    fn interner(&self) -> &StringInterner {
+        &self.interner
+    }
+
+    fn common(&self) -> &CommonIdentifiers {
+        &self.common
+    }
+
+    fn diagnostic_handler(&self) -> &Arc<dyn DiagnosticHandler> {
+        &self.diagnostic_handler
+    }
+}
+
+#[cfg(test)]
+pub mod test_helpers {
+    use super::*;
+    use crate::cli::diagnostics::TestDiagnosticHandler;
+    use typedlua_parser::string_interner::StringInterner;
+
+    pub fn create_test_context() -> TypeCheckContextImpl {
+        let interner = Arc::new(StringInterner::new());
+        let common = Arc::new(CommonIdentifiers::new(&interner));
+        let diagnostic_handler: Arc<dyn DiagnosticHandler> = Arc::new(TestDiagnosticHandler::new());
+        TypeCheckContextImpl::new(interner, common, diagnostic_handler)
+    }
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,4 @@
+pub mod context;
 pub mod type_checker;
 pub mod type_compat;
 pub mod type_environment;
